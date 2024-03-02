4456fa99d4c4f403cf43d40d3b69682044f36b90
Shah Ji <shahji1998@gmail.com>
2024-03-02 11:50:26 +0500

    Update pre_gen.dart

--- bricks/coverage_badge/hooks/pre_gen.dart
+++ bricks/coverage_badge/hooks/pre_gen.dart
@@ -1,15 +1,20 @@
 import 'package:mason/mason.dart';
 
 void run(HookContext context) {
-  Map<String, dynamic> variables= context.vars;
-  String percentage = variables['percentage'].toString();
-  String svgColor='0FCF00';
-  if(num.parse(percentage)<50){
-    svgColor='FF0000';
+  try{
+    Map<String, dynamic> variables= context.vars;
+    String percentage = variables['percentage'].toString();
+    String svgColor='0FCF00';
+    if(num.parse(percentage)<50){
+      svgColor='FF0000';
+    }
+    else if(num.parse(percentage)<80){
+      svgColor='FFCF00';
+    }
+    variables['badgeColor']=svgColor;
+    context.vars=variables;
+  } catch(error,stack){
+    context.logger.err(error.toString());
+    context.logger.err(stack.toString());
   }
-  else if(num.parse(percentage)<80){
-    svgColor='FFCF00';
-  }
-  variables['badgeColor']=svgColor;
-  context.vars=variables;
 }
